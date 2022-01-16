a63b0bd6c1c6002f1caf2e8b6285f632614a09a4
Chuma <lokl0k17@mail.ru>
2022-01-16 21:11:53 +0700

    Задний фон

--- main.py
+++ main.py
@@ -7,13 +7,23 @@ screen = pygame.display.set_mode((288, 512))
 
 
 def load_image(name):
-    fullname = os.path.join('assets', name)
+    fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
     return image
 
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 450))
+    screen.blit(floor_surface, (floor_x_pos + 288, 450))
+
+
+bg_surface = load_image('background.png')
+
+floor_surface = load_image('base.png')
+floor_x_pos = 0
 
 clock = pygame.time.Clock()
 running = True
@@ -23,6 +33,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.blit(bg_surface, (0, 0))
+
+    floor_x_pos -= 5
+    draw_floor()
+    if floor_x_pos <= -288:
+        floor_x_pos = 0
+
     pygame.display.update()
     clock.tick(120)
 pygame.quit()
